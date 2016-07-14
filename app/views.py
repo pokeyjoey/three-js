30c95abc2c917ab3e83205a413e71564b2c813c6
Jeff Roche <jkeauctions@gmail.com>
2016-07-14 07:58:08 -0400

    Code up to Earth rotating with lighting.

--- app/views.py
+++ app/views.py
@@ -6,3 +6,29 @@ from app import app
 def basic_scene():
     return render_template('01.01-basic-scene.html', title='Basic Scene')
 
+@app.route('/simple_mesh')
+def simple_mesh():
+    return render_template('01.02-simple-mesh.html', title='Simple Mesh')
+
+@app.route('/vertices')
+def vertices():
+    return render_template('01.03-vertices.html', title='vertices')
+
+# Chapter 2
+@app.route('/advanced_textures')
+def advanced_textures():
+    return render_template('02.05-advanced-textures.html', title='advanced_textures')
+
+@app.route('/globe_and_camera')
+def globe_and_camera():
+    return render_template('02.01-globe-and-camera.html', title='globe_and_camera')
+
+@app.route('/basic_textures')
+def basic_textures():
+    return render_template('02.02-basic-textures.html', title='basic_textures')
+
+@app.route('/add_lighting')
+def add_lighting():
+    return render_template('02.03-add-lighting.html', title='add_lighting')
+
+
